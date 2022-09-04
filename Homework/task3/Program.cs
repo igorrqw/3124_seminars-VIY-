09dde5632c5b57aa56ada58c70a697e1f14ec6e1
Василенко Игорь <warface.777@mail.ru>
2022-09-04 23:17:54 +0300

    Homework lesson 1

---
+++ Homework/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+int a;
+Console.WriteLine("Введите одно число:");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
+Console.ReadLine();
