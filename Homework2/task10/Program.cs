590bac0e9a4703bf02bedde86b26606c83f33bd4
Василенко Игорь <warface.777@mail.ru>
2022-09-07 21:06:33 +0300

    Homework2

---
+++ Homework2/task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+
+Random rand = new Random();  
+int number = rand.Next(100, 1000);
+Console.WriteLine($"Сгенирировать число {number}");
+Console.WriteLine(number);
+int digit = number;
+digit = (number / 10) % 10; 
+Console.WriteLine(digit);
