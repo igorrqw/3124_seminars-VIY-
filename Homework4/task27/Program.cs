daf533dc875569a44e97fccaac154e1d9e8bc2a6
Василенко Игорь <warface.777@mail.ru>
2022-09-15 21:19:28 +0300

    Homework4

---
+++ Homework4/task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// // 9012 -> 12
+
+
+int ReadNumber(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());  
+}
+
+int numberOfDigits(int num) 
+{ 
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int namber = ReadNumber("Введите число:");
+int num = numberOfDigits(namber);
+Console.WriteLine($"Сумма цифр числа :  {num}");
---
+++ Homework4/task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int i;
+int[] array = new int[8];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 8; i++)
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (i = 0; i < 8; i++)
+Console.Write(array[i] + " ");
