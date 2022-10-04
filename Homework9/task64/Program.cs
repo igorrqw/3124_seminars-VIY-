3be06d0bb0e531bdc5b70358157582fda2e5cbee
Василенко Игорь <warface.777@mail.ru>
2022-10-04 11:26:12 +0300

    Homework9

---
+++ Homework9/task64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintToB(int b)
+{
+    if (b <= 0)
+    {
+        return;
+    }
+    else 
+    {
+        Console.Write(b + " ");
+        PrintToB(b - 1);
+    }
+}
+ 
+int b =  ReadNumber("Введите число N: ");
+PrintToB(b);
---
+++ Homework9/task66/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int sum = 0;
+void PrintToB(int a, int b )
+{
+    if (a > b)
+    {
+        return;
+    }
+    else 
+    {
+        PrintToB(a + 1, b);
+        sum = sum + a;
+    }
+}
+
+int a =  ReadNumber("Введите число M: ");
+int b =  ReadNumber("Введите число N: ");
+PrintToB(a,b);
+Console.Write(sum);
+
+// int sum = 0;
+// void PrintToB(int a, int b )
+// {
+//     if (b < a)
+//     {
+//         return;
+//     }
+//     else 
+//     {
+//         PrintToB(a,b - 1);
+//         sum = sum + b;
+//     }
+// }
+
+// int a =  ReadNumber("Введите число M: ");
+// int b =  ReadNumber("Введите число N: ");
+// PrintToB(a,b);
+// Console.Write(sum);
+
+
+
+
+// ===========
+// int ReadNumber(string message)
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int sum = 0;
+// int m =  ReadNumber("Введите число M: ");
+// int n =  ReadNumber("Введите число N: ");
+
+// for (int i = m; i <= n; i++)
+// {
+//     sum = sum + i;
+// }
+
+// Console.Write($"-> {sum}");
